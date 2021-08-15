79314be403ec4a12129aec98afb4026169d3a4c8
Inkee <68136101+Inkeee@users.noreply.github.com>
2021-08-14 21:23:30 -0300

    Update index.js

--- exemplos/slash-commands/index.js
+++ exemplos/slash-commands/index.js
@@ -10,32 +10,25 @@ client.on("ready", async () => {
   
    for (const files of arquivos) {
      
-       const file = require(`./src/slashs/${files}`);
+      const file = require(`./src/slashs/${files}`);
 	  
-	     slash.push(file)
+      slash.push(file)
 
-       await client.api.applications(client.user.id).commands.post({ data: file.data })
+      await client.api.applications(client.user.id).commands.post({ data: file.data })
+
    }
   
-   client.ws.on('INTERACTION_CREATE', async (i) => {
-      
-      var command = slash.find(x => x.data.name === i.data.name.toLowerCase())
-	    
-      if(command) command.run(client, send, i)
-	    
-   })
+
+   client.on('interactionCreate', async (i) => {
+     
+      if(!i.isCommand()) return;
 	 
-   async function send(interaction, content) {
-      const { data, files } = await Discord.APIMessage.create(client.channels.resolve(interaction.channel_id), content)
-      .resolveData().resolveFiles();
-	  
-      return client.api.interactions(interaction.id, interaction.token).callback.post({
-         data: {
-            type: 4,
-	    data: { ...data, files: files }
-	 }, files
-      });
-   }
+      var command = slash.filter(x => x.data.name === i.commandName)
+      
+      if(command) command[0].run(client, i)
+
+   })
+
 })
 
 client.login(process.env.TOKEN);
