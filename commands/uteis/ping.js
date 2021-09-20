de04186bf67d527bc0adddc81923893713caf3bf
Inkee <68136101+Inkeee@users.noreply.github.com>
2021-09-19 23:49:18 -0300

    Create ping.js

---
+++ commands/uteis/ping.js
@@ -0,0 +1,21 @@
+
+//base de um comando
+
+const Discord = require("discord.js")
+
+module.exports = {
+  config: {
+    name: "ping", //nome do comando
+    aliases: ["pong"], //sinônimos do comando
+    category: "Utilidades", // categoria do comando
+    cooldown: 10, // tempo para acrescentar no cooldown
+    permission: [] // permissões para executar o comando!
+    // nas permissões use USER_ para permissões de usuários e sem nada para permissões do bot 
+    //exemplo: user - USER_MANAGE_GUILD, bot - MANAGE_GUILD
+  },
+  run: async (client, message, args) => {
+
+  message.reply({content: "pong"})
+  
+  }
+}
